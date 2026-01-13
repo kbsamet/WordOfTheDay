f5fc7ff51246ce29bc78d62f87ed7c4cb192e2dd
Koray Samet Küçükbayraktar <samet.kucukbayraktar@aksis.com.tr>
2026-01-13 13:14:06 +0300

    Initial Commit

---
+++ Word Of The Day/Word_Of_The_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Of_The_DayApp.swift
+//  Word Of The Day
+//
+//  Created by Koray Samet Kucukbayraktar on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Word_Of_The_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
